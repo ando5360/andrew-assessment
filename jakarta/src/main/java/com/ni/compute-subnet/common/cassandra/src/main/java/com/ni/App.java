61145b64d7eaac5613fe69627839a0dfaf968ac5
Andrew Thomson <thomson53@outlook.com>
2024-02-05 00:46:44 +0000

    initial project structure for conceptual purpose

---
+++ jakarta/src/main/java/com/ni/compute-subnet/common/cassandra/src/main/java/com/ni/App.java
@@ -0,0 +1,13 @@
+package com.ni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jakarta/src/main/java/com/ni/compute-subnet/components/validator/validate.java
@@ -0,0 +1,2 @@
+//calculate gpu readings in java this isnt nesc private data and I beleive acttack vectore to be limited
+//still follow security protocol
